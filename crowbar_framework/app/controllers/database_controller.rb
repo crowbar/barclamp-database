42d69092fb615e1acd8a401494c6a44283f8a2aa
Thomas Boerger <tboerger@suse.de>
2014-05-13 17:09:00 +0200

    Added strong parameters method

--- crowbar_framework/app/controllers/database_controller.rb
+++ crowbar_framework/app/controllers/database_controller.rb
@@ -23,4 +23,8 @@ class DatabaseController < BarclampController
   def initialize_service
     @service_object = DatabaseService.new logger
   end
+
+  def permitted_params
+    params.require(:database)
+  end
 end

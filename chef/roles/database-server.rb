5cf7164764a52e73a44232db1cb52a3b3f8df108
Vincent Untz <vuntz@suse.com>
2015-09-09 11:31:27 +0200

    Add note that this got moved to merged repo

--- chef/roles/database-server.rb
+++
@@ -1,9 +0,0 @@
-name "database-server"
-description "Database Server Role"
-run_list(
-         "recipe[database::crowbar]",
-         "recipe[database::server]"
-)
-default_attributes()
-override_attributes()
-
